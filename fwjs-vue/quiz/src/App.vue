21087780f36444d4b2656b427ca738c12733adc1
ianbarreto22 <61208086+ianbarreto22@users.noreply.github.com>
2022-04-19 22:40:13 -0300

    Add files via upload

---
+++ fwjs-vue/quiz/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import TabPanel from './components/TabPanel.vue'
+
+</script>
+
+<template>
+  <TabPanel></TabPanel>
+</template>
+
+<style>
+@import './assets/base.css';
+</style>
---
+++ fwjs-vue/quiz/src/components/TabPanel.vue
@@ -0,0 +1,57 @@
+<template>
+    <div className="panel">
+        <Tab v-for="(tab, index) in tabs"
+            :key="index"
+            :index="index"
+            v-bind="tab"
+            :selected="currentTab"
+            @on-selection="select">
+        </Tab>
+
+        <div className="box">
+            <p>{{tabs[currentTab].content}}</p>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import {ref} from 'vue'
+    import Tab, {type TabProps} from './Tab.vue'
+
+    const tabs: TabProps[] = [
+    {
+      title: "Tab 1",
+      content: "Conteúdo da primeira tab!"
+    },
+    {
+      title: "Tab 2",
+      content: "Conteúdo da segunda tab!"
+    },
+    {
+      title: "Tab 3",
+      content: "Conteúdo da terceira tab!"
+    },
+    {
+      title: "Tab 4",
+      content: "Conteúdo da quarta tab!"
+    },
+  ]
+
+  const currentTab = ref(0)
+
+  function select(tabIndex: number){
+      currentTab.value = tabIndex
+  }
+</script>
+
+<style>
+    div .panel{
+        margin: 100px;
+    }
+
+    .box{
+        border: 3px solid lightslategray;
+        border-radius: 0px 10px 10px 10px;
+        padding: 15px;
+    }
+</style>
